f9d5d599b51571c0e9f9c480877e9aa2d755e4a9
joaogsleite <joao.leite@isobar.com>
2021-05-02 17:38:36 +0100

    fix latest tweet widget

--- components/LatestTweet/index.tsx
+++ components/LatestTweet/index.tsx
@@ -2,7 +2,7 @@
 export function LatestTweet() {
   return (
     <div className="LatestTweet">
-      <a className="twitter-timeline" data-lang="pt" data-theme="light" href="https://twitter.com/6uilhermeLeite?ref_src=twsrc%5Etfw"></a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      <a className="twitter-timeline" data-lang="pt" data-theme="light" href="https://twitter.com/6uilhermeLeite?ref_src=twsrc%5Etfw"></a> <script async src="https://platform.twitter.com/widgets.js"></script>
     </div>
   )
 }
