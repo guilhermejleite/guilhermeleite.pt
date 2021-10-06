30f5c767eeabc61ac912167cdeb7414b3e91d46d
joaogsleite <joaogsleite@users.noreply.github.com>
2021-10-06 18:20:29 +0000

    Deploy to GitHub pages

---
+++ _next/static/SaylPWuVhuQZ2W-_vPMtd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-120e1d8fe45be373f6e8.js"],"/_error":["static/chunks/pages/_error-1bc66c7a29b44b52374c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
